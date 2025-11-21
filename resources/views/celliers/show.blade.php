a3391a024a14b3ccc0549f61d7afd337206ab652
Philippe Cossette <philcossette@hotmail.com>
2025-11-21 08:50:05 -0500

    Changement du design

--- resources/views/celliers/show.blade.php
+++ resources/views/celliers/show.blade.php
@@ -88,7 +88,7 @@
     </div>
 
 </section>
-<div id='addWineBtnContainer' class="fixed z-10 bottom-0 left-0 w-full p-4 pt-10 bg-card border border-border-base shadow-lg rounded-t-lg transform translate-y-full transition-transform duration-300">
+<div id='addWineBtnContainer' class="fixed z-10 bottom-0 left-0 w-full p-4 py-10 bg-card border border-border-base shadow-lg rounded-t-lg transform translate-y-full transition-transform duration-300">
     <span class="flex items-center justify-between mb-4"> 
        <h1 class="text-3xl text-heading font-heading">Ajouter un vin</h1> 
        <x-dynamic-component :component="'lucide-x'" id="closeAddWine" class="w-6 h-6"/>
